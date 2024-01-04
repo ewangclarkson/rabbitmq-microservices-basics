97eee2692a441475a8fb1ce432eb78985fa437b6
ewangclarkson <ewangclarks@gmail.com>
2024-01-04 14:14:22 +0100

    fix big

--- rabbitmq-consumer/src/main/java/com/ewangclarks/mqc/RabbitmqConsumerApplication.java
+++ rabbitmq-consumer/src/main/java/com/ewangclarks/mqc/RabbitmqConsumerApplication.java
@@ -1,12 +1,12 @@
 package com.ewangclarks.mqc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan(basePackages = "com.ewangclarks.*")
+@ConfigurationPropertiesScan(basePackages = "com.ewangclarks.mqc.*")
 public class RabbitmqConsumerApplication {
 
 	public static void main(String[] args) {
